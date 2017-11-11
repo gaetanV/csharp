17dde325be80cb08a0eb5b65840b20922109a5ab
gaetanV <gaetan.vigneron@gmail.com>
2017-11-12 00:49:23 +0100

    Init c# collection

---
+++ git/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace git
+{
+    class Program
+    {
+        private static async Task ProcessRepositories()
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("User-Agent", "Github Travis");
+            var stringTask = client.GetStringAsync("https://api.github.com/users/gaetanV");
+            var msg = await stringTask;
+            Console.Write(msg);
+        }
+
+        static void Main(string[] args)
+        {
+            ProcessRepositories().Wait();
+        }
+    }
+}
