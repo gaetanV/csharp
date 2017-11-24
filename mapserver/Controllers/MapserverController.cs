009de2bf7a9168f4c8d4cbc2a4fe6aa205ed1fd2
gaetanV <gaetan.vigneron@gmail.com>
2017-11-24 19:45:55 +0100

    Init mapserver

---
+++ mapserver/Controllers/MapserverController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Web;
+
+namespace mapserver.Controllers
+{
+    [Route("mapserver/")]
+    public class ValuesController : Controller
+    {
+
+        [HttpGet("wfs/{x}/{y}")]
+        public ActionResult GetWfs(int x,int y)
+        {   
+            if(y>2 || y<0 || x>2 || x<0 ){
+               return Content("ERROR!");
+            }
+            return File(System.IO.File.ReadAllBytes($"./Ressources/{x}.jpg"), "image/jpg");
+        }
+
+        [HttpGet("wms")]
+        public ActionResult GetWms(int? x,int? y)
+        {   
+            if(y == null || y>2 || y<0 || x == null || x>2 || x<0 ){
+               return Content("ERROR!");
+            }
+            return File(System.IO.File.ReadAllBytes($"./Ressources/{x}.jpg"), "image/jpg");
+        }
+    }
+}
